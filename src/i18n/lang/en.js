1629be77ee2a53d7e5c5cc16b496b2edbd2b9726
wangsitu <996803783@qq.com>
2018-10-15 12:10:58 +0800

    增加i18

---
+++ src/i18n/lang/en.js
@@ -0,0 +1,13 @@
+const en = {
+  i18nView: {
+    language: 'Language',
+    account: 'Account',
+    text: 'AirGrab is a way of distributing candy that consumes the user\'s memory (about 0.25KB per token). After executing AirGrab, we need to wait for the airdrop of the project side. For specific airdrop time, please see the introduction and official website of each project.',
+    synopsis: 'Synopsis',
+    keyword: 'Keyword',
+    creation: 'Creation account, airdrop ratio 1:1',
+    data: 'Data source of this tool'
+  }
+};
+
+export default en;
